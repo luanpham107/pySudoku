538d20ea012ed6fcb7cde046adf84475349399db
Liam <liam@enclave.vn>
2018-12-20 17:35:06 +0700

    change to grid

--- SudokuMain.py
+++ SudokuMain.py
@@ -1,36 +1,58 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec 10 11:12:40 2018
-@author: Liam
-"""
-
-from PyQt5 import uic, QtWidgets
-from PyQt5 import Qt
-from PyQt5.QtWidgets import (QPushButton, QApplication, QTableWidget)
-
 import sys
-        
-class Ui(QtWidgets.QDialog):
-    
+from pprint import pprint
+from PyQt5.QtWidgets import (QWidget, QGridLayout,QPushButton, QApplication)
+
+class TutorialWindow(QWidget):
     def __init__(self):
-        super(Ui, self).__init__()
-        uic.loadUi('guiTable.ui', self)
-        #self.line = self.findChild(QPushButton, 'pushButton_50')                
-        table1 = self.findChild(QTableWidget, 'tableWidget')
-        print(table1.currentRow())
-        table1.clicked.connect(self.mouseClickOnTable_handler)
-        self.show()
+        super().__init__()
+        grid_layout = QGridLayout()
+        self.setLayout(grid_layout)        
+        lines = self.readPuzzle("puzzle.txt");  
         
-    def mouseClickOnTable_handler(self):
-        print('Mouse Clicked')
+        print(lines)
         
-    def myfunction(self):
-        print('enter my function')
+        values = [
+            '1', '1', '1', '1', '1', '1', '1', '1', '1',
+            '1', '1', '1', '1', '1', '1', '1', '1', '1',
+            '1', '1', '1', '1', '1', '1', '1', '1', '1',
+            '1', '1', '1', '1', '1', '1', '1', '1', '1',
+            '1', '1', '1', '1', '1', '1', '1', '1', '1',
+            '1', '1', '1', '1', '1', '1', '1', '1', '1',
+            '1', '1', '1', '1', '1', '1', '1', '1', '1',
+            '1', '1', '1', '1', '1', '1', '1', '1', '1',
+            '1', '1', '1', '1', '1', '1', '1', '1', '1',            
+        ]
         
-    def keyPressEvent(self, e):
-        if e.key() == Qt.KeyEscape:
-            self.close()
+        for i in range(9):
+            for j in range(9):
+                values[i*9 + j] = lines[i][j]
+        
+        print (values)
+        positions = [(i, j) for i in range(9) for j in range(9)]
+        # position is an array of tuples
+        #pprint("positions = "+str(positions))
+        self.setGeometry(300, 300, 300, 120)
+        for position, value in zip(positions, values):
+            print("position = " + str(position))
+            print("value = " + str(value))
+            if value == '':
+                continue
+            button = QPushButton(value)            
+            grid_layout.addWidget(button, *position)
+        self.setWindowTitle('Sudoku')
+        
+    def readPuzzle(self, strfileName):
+        with open(strfileName, "r") as ins:
+            array = []
+            for line in ins:
+                lineX = line.split('\n')                
+                lineY = lineX[0].split(',')
+                array.append(lineY)        
+        return array
+
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    window = Ui()
+    app = QApplication(sys.argv)
+    pprint("input parameters = " + str(sys.argv))
+    tutorial_window = TutorialWindow()
+    tutorial_window.show()
     sys.exit(app.exec_())
